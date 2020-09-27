330c1d7a6d420c4a64e54755a24164e4b624fa82
juanogit <jcarayaf64@gmail.com>
2020-09-27 11:39:41 +0200

    - En pachage.json se agrega el --host 0.0.0.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InicioComponent} from './inicio/inicio/inicio.component';
+import {QuienesSomosComponent} from './nosotros/quienes-somos/quienes-somos.component';
+import {TratamientosComponent} from './tratamientos/tratamientos/tratamientos.component';
+import {PromocionesComponent} from './promociones/promociones/promociones.component';
+import {ContactosComponent} from './contactos/contactos/contactos.component';
+import {CommonModule} from '@angular/common';
+import {TratamientosCorporalesComponent} from './tratamientos/tratamientos-corporales/tratamientos-corporales.component';
+import {TratamientosFacialesComponent} from './tratamientos/tratamientos-faciales/tratamientos-faciales.component';
+import {TratamientosRelajacionComponent} from './tratamientos/tratamientos-relajacion/tratamientos-relajacion.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'quienesSomos', component: QuienesSomosComponent },
+  { path: 'tratamientos', component: TratamientosComponent },
+  { path: 'tratamientosCorporales', component: TratamientosCorporalesComponent },
+  { path: 'tratamientosFaciales', component: TratamientosFacialesComponent },
+  { path: 'tratamientosRelajacion', component: TratamientosRelajacionComponent },
+  { path: 'promociones', component: PromocionesComponent },
+  { path: 'contacto', component: ContactosComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'inicio' },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './templates/menu/menu.component';
+import { InicioComponent } from './inicio/inicio/inicio.component';
+import { QuienesSomosComponent } from './nosotros/quienes-somos/quienes-somos.component';
+import { TratamientosComponent } from './tratamientos/tratamientos/tratamientos.component';
+import { ContactosComponent } from './contactos/contactos/contactos.component';
+import { PromocionesComponent } from './promociones/promociones/promociones.component';
+import {AngularMaterialModule} from './angular-material-module';
+import { SidenavListComponent } from './templates/sidenav-list/sidenav-list.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { TratamientosCorporalesComponent } from './tratamientos/tratamientos-corporales/tratamientos-corporales.component';
+import { TratamientosFacialesComponent } from './tratamientos/tratamientos-faciales/tratamientos-faciales.component';
+import { TratamientosRelajacionComponent } from './tratamientos/tratamientos-relajacion/tratamientos-relajacion.component';
+import { FooterComponent } from './templates/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    InicioComponent,
+    QuienesSomosComponent,
+    TratamientosComponent,
+    ContactosComponent,
+    PromocionesComponent,
+    SidenavListComponent,
+    TratamientosCorporalesComponent,
+    TratamientosFacialesComponent,
+    TratamientosRelajacionComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    LayoutModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
